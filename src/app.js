78cfa6b4809e71269881213f12cb5a25267a7781
Prakash-2422 <prakash2404.bot@gmail.com>
2025-05-26 13:12:00 +0530

    exploring GET, POST, DELETE

--- src/app.js
+++ src/app.js
@@ -2,6 +2,24 @@ const express = require("express");
 
 const app = express();
 
+// app.use('/',(req, res)=>{
+//     res.send(("Heloo from Initial..."))
+// })
+
+app.get('/user', (req, res)=>{
+    res.send({firstNAme:"Prakash", lastName:"Raj"})
+})
+
+app.post('/user',(req,res)=>{
+    console.log(req.body);
+    
+    res.send("Posted User Successfully")
+})
+
+app.delete('/user',(req,res)=>{
+    res.send("Deleted Successfully....!")
+})
+
 app.use('/test',(req, res)=>{
     res.send(("Heloo from the server..."))
 })
@@ -10,9 +28,6 @@ app.use('/hello',(req, res)=>{
     res.send(("Heloo Heloo Hellloo..."))
 })
 
-app.use('/',(req, res)=>{
-    res.send(("Heloo from Initial..."))
-})
 
 app.listen(7777, ()=>{
     console.log("Server is Successfully listening on port 3000....")
